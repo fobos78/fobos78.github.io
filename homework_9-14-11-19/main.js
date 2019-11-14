abf651dbb75f7752e3c9bd8ff0ca06d5a87b8315
fobos78 <57541130+fobos78@users.noreply.github.com>
2019-11-14 21:47:10 +0300

    + домашка9
    У нас в наших faq - панельках есть стрелочки. Сделайте так, что бы они поворачивались на 180 градусов при клике, и наоборот.  Стоит использовать анимации из animate.css

---
+++ homework_9-14-11-19/main.js
@@ -0,0 +1,82 @@
+// при клике на красную  линию увеличивается плавно сама линия
+
+
+
+
+let ck; //undefined
+let card__lines = document.querySelectorAll('.card__line');
+let ck_warning = document.querySelector('.ck_warning');
+let cross = document.querySelectorAll('.ck_warning__cross');
+let promo_form = document.querySelector('.form_wrapper');
+let action__btn = document.querySelector('.action__btn');
+let def_animation = 'fadeOutUp';
+let faq_quest = document.querySelectorAll(".faq__panel__quest");
+let faq_answer = document.querySelectorAll(".faq__panel__answer");
+let fa = document.querySelectorAll(".fa");
+
+
+
+var flag = true;
+
+for (let i = 0; i < faq_quest.length; i++) {
+
+    
+
+    faq_quest[i].onclick = () => {
+
+        
+        faq_answer[i].classList.add("animated");
+        if(flag){
+            fa[i+1].classList.remove('fa-angle-up');
+            fa[i+1].classList.add('fa-angle-down');
+            faq_answer[i].classList.remove("zoomOut");
+            faq_answer[i].classList.add("zoomIn");
+            faq_answer[i].classList.add("faq_helper");
+            flag = false;
+        } 
+        else {
+            fa[i+1].classList.remove('fa-angle-down');
+            fa[i+1].classList.add('fa-angle-up');
+            faq_answer[i].classList.remove("zoomIn");
+            faq_answer[i].classList.add("zoomOut");
+            flag = true;
+            setTimeout(() => {
+                faq_answer[i].classList.remove("faq_helper");
+            }, 400);
+        }
+
+    }
+}
+
+
+for (let i = 0; i < card__lines.length; i++) {
+    card__lines[i].onclick = function(){
+        this.classList.toggle('card__line__helper');
+    }
+}
+
+
+cross[1].onclick = () => {
+    ck_warning.style.display = "none";
+    // ck_warning.remove();
+}
+
+action__btn.onclick = () => {
+    if(promo_form.classList.contains(def_animation)){
+        promo_form.classList.remove(def_animation);
+    }
+    promo_form.style.display = "flex"; 
+    // alert("по мне кликнили");
+}
+
+cross[0].onclick = () => {
+    promo_form.classList.add(def_animation);
+    setTimeout(() => {
+        promo_form.style.display = "none";
+    }, 400);
+    
+    // ck_warning.remove();
+}
+// у нас есть форма, которая изначально не видна
+//при клике на кнопку присоединится она появится, при нажатии на крестик пропадет
+//через 5 секунд на сайте она автоматически появляется
